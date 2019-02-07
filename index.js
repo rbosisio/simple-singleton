80d897c54df92db6354f1e10810f6733f8ebd3cb
rbosisio <rod.bosisio@gmail.com>
2019-02-06 22:17:22 -0200

    adding instances to a list in super class

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 class Super {
     constructor(){
-        this._instance
+        this._instances = {}
     }
     
     get Singleton() {
         const self = this;
         return class Singleton {
             constructor() {
-                if (self._instance) throw new Error();
-                self._instance = this;
+                if (self._instances[this.constructor.name]) throw new Error();
+                self._instances[this.constructor.name] = this;
             }
         
             static getInstance(...args) {
-                return self._instance || new this(...args);
+                return self._instances[this.name] || new this(...args);
             }
         }
     }
 }
 
-module.exports = { getClass: () => new Super().Singleton };
+module.exports = new Super().Singleton;
